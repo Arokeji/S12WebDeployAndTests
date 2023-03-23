933eac3d06f2e2a29c16121e931872546224c5c0
Humberto CL <pennywiser23@hotmail.com>
2023-03-23 23:07:14 +0100

    ejercicio iniciado

---
+++ src/components/Slider/Slider.test.js
@@ -0,0 +1,19 @@
+test("renders without crashing", () => {
+  // TODO
+});
+
+test("displays the first image on initial render", () => {
+  // TODO
+});
+
+test('changes the displayed image when the "Siguiente" button is clicked', () => {
+  // TODO
+});
+
+test('changes the displayed image when the "Anterior" button is clicked', () => {
+  // TODO
+});
+
+test("displays images with the correct src", () => {
+  // TODO
+});
